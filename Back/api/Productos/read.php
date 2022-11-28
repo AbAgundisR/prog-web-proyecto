0b721f9180758300af0d4d977060acba7aea9d76
AAgundis <aagundisr@gmail.com>
2022-11-28 10:16:41 -0600

    [Update] error checking

--- Back/api/Productos/read.php
+++ Back/api/Productos/read.php
@@ -5,44 +5,65 @@ header("Content-Type: application/json; charset=UTF-8");
 header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Content-Range, Content-Disposition, Content-Description');
 
-// database connection will be here
-// include database and object files
-include_once '../_Config/config.php';
+// ini_set("log_errors", 1);
+// ini_set("error_log", "./php-error.log");
+// error_log("Hello, errors!");
 
-// instantiate database and product object
-$database = new Database();
-$db = $database->getConnection();
+try {
+    // database connection will be here
+    // include database and object files
+    include_once '../_Config/config.php';
 
-// Prepare a select statement
-$sql = "SELECT * FROM Productos";
+    file_put_contents("./php-error.log", "1", FILE_APPEND);
 
-if ($result = mysqli_query($db, $sql)) {
-    $productos_arr = array();
-    $productos_arr["productos"] = array();
+    // instantiate database and product object
+    $database = new Database();
+    $db = $database->getConnection();
 
-    if (mysqli_num_rows($result) > 0) {
-        while ($row = mysqli_fetch_array($result)) {
-            extract($row);
+    // Prepare a select statement
+    $sql = "SELECT * FROM Productos";
 
-            $producto_item = array(
-                "ID" => $ID,
-                "nombre" => $nombre,
-                "precio" => $precio,
-                "descripcion" => $descripcion,
-                "in_stock" => $in_stock,
-                "imagen" => $imagen,
-                "blanco_negro" => $blanco_negro
-            );
+    file_put_contents("./log.log", "2", FILE_APPEND);
 
-            array_push($productos_arr["productos"], $producto_item);
+    if ($result = mysqli_query($db, $sql)) {
+
+        file_put_contents("./log.log", "3", FILE_APPEND);
+
+        $productos_arr = array();
+        $productos_arr["productos"] = array();
+
+        if (mysqli_num_rows($result) > 0) {
+
+            file_put_contents("./log.log", "4", FILE_APPEND);
+
+            while ($row = mysqli_fetch_array($result)) {
+                file_put_contents("./log.log", "5", FILE_APPEND);
+
+                extract($row);
+
+                $producto_item = array(
+                    "ID" => $ID,
+                    "nombre" => $nombre,
+                    "precio" => $precio,
+                    "descripcion" => $descripcion,
+                    "in_stock" => $in_stock,
+                    "imagen" => $imagen,
+                    "blanco_negro" => $blanco_negro
+                );
+
+                array_push($productos_arr["productos"], $producto_item);
+            }
+
+            // set response code - 200 OK
+            http_response_code(200);
+
+            // show products data in json format
+            echo json_encode($productos_arr);
         }
-
-        // set response code - 200 OK
-        http_response_code(200);
-
-        // show products data in json format
-        echo json_encode($productos_arr);
+    } else {
+        echo 'a';
     }
-} else {
-    echo 'a';
+} catch (Exception $th) {
+    echo $th;
+    //throw $th;
 }
